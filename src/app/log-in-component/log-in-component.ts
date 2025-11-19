4cd3fcdc2c8596e44415ecf8912926e4e6091e56
JMSB-LEARN <jmarquess04@iesalbarregas.es>
2025-11-19 16:17:48 +0000

    Setting Up

---
+++ src/app/log-in-component/log-in-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-component',
+  imports: [],
+  templateUrl: './log-in-component.html',
+  styleUrl: './log-in-component.css',
+})
+export class LogInComponent {
+
+}
